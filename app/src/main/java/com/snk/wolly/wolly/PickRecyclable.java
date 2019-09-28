f7295d4b96e8abdc5d104c40ef18a2c09544e864
coldblade2000 <diegogranada7@gmail.com>
2019-09-28 17:29:09 -0500

    FINAL PUSH

--- app/src/main/java/com/snk/wolly/wolly/PickRecyclable.java
+++ app/src/main/java/com/snk/wolly/wolly/PickRecyclable.java
@@ -1,5 +1,6 @@
 package com.snk.wolly.wolly;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +13,8 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 //import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+import com.bumptech.glide.Glide;
 
 import java.util.Random;
 
@@ -28,13 +31,48 @@ public class PickRecyclable extends AppCompatActivity {
     TextView tvProfileLevel, tvBarrioLevel, tvBarrioNameMain, tvProfileNameMain;
     float dpWidth;
 
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String id = "ivBaterias";
+            switch (v.getId()){
+                case R.id.ivBaterias:
+                    id = "ivBaterias";
+                    break;
+                case R.id.ivBottle:
+                    id = "ivBottle";
+                    break;
+                case R.id.ivPaper:
+                    id = "ivPaper";
+                    break;
+                case R.id.ivCap:
+                    id = "tvCap";
+                    break;
+                case R.id.ivTetrapak:
+                    id = "ivTetrapak";
+                    break;
+                case R.id.ivGlassBottle:
+                    id = "ivGlassBottles";
+                    break;
+            }
+            clickAdd(id);
+        }
+    };
+
+    private void clickAdd(String id) {
+        Intent intent = new Intent(this, AddRecycle.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("id", id);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_recyclable);
         Random rand = new Random();
-        defaultBarrio = new Barrio(rand.nextInt(100*100), "Barrio Santa Paula");
-        defaultProfile = new Profile((rand.nextInt(100*100)), "Jose Manuel Florez");
+        defaultBarrio = new Barrio(rand.nextInt(1000*100), "Barrio Santa Paula");
+        defaultProfile = new Profile((rand.nextInt(1000*100)), "Jose Manuel Florez");
 
 
 
@@ -42,6 +80,7 @@ public class PickRecyclable extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ivRecycleLogo = (CircleImageView) findViewById(R.id.ivRecycleLogo);
+
         ivBottle= (CircleImageView) findViewById(R.id.ivBottle);
         ivPaper = (CircleImageView) findViewById(R.id.ivPaper);
         ivCap = (CircleImageView) findViewById(R.id.ivCap);
@@ -50,6 +89,27 @@ public class PickRecyclable extends AppCompatActivity {
         ivBaterias = (CircleImageView) findViewById(R.id.ivBaterias);
         ivBarrioMain = (CircleImageView) findViewById(R.id.ivBarrioMain);
         ivProfileMain = (CircleImageView) findViewById(R.id.ivProfileMain);
+
+        Glide.with(this)
+                .load(R.mipmap.bottle)
+                .into(ivBottle);
+        Glide.with(this)
+                .load(R.mipmap.paper)
+                .into(ivPaper);
+        Glide.with(this)
+                .load(R.mipmap.cap)
+                .into(ivCap);
+        Glide.with(this)
+                .load(R.mipmap.tetra)
+                .into(ivTetrapak);
+        Glide.with(this)
+                .load(R.mipmap.glass)
+                .into(ivGlassBottle);
+        Glide.with(this)
+                .load(R.mipmap.battery)
+                .into(ivBaterias);
+
+
 
         flBarrioProgressMain = findViewById(R.id.flBarrioProgressMain);
         flProfileProgressMain = findViewById(R.id.flProfileProgressMain);
@@ -64,21 +124,33 @@ public class PickRecyclable extends AppCompatActivity {
 
         tvProfileNameMain.setText(defaultProfile.getName());
         tvBarrioNameMain.setText(defaultBarrio.getName());
-        tvProfileLevel.setText("LVL " + (defaultProfile.getPuntos()/100));
-        tvBarrioLevel.setText("LVL " + (defaultBarrio.getPuntos()/100));
+        tvProfileLevel.setText("LVL " + (defaultProfile.getPuntos()/1000));
+        tvBarrioLevel.setText("LVL " + (defaultBarrio.getPuntos()/1000));
 
         DisplayMetrics displayMetrics = getApplicationContext().getResources().getDisplayMetrics();
         dpWidth = displayMetrics.widthPixels;
+        setMargins(flBarrioProgressMain, 0,0,(int)(defaultBarrio.getPuntos()%1000 *dpWidth/1000),0);
+        setMargins(flProfileProgressMain, 0,0,(int)(defaultProfile.getPuntos()%1000 *dpWidth/1000),0);
 
-
+        ivRecycleLogo.setOnClickListener(clickListener);
+        ivBottle.setOnClickListener(clickListener);
+        ivPaper.setOnClickListener(clickListener);
+        ivCap.setOnClickListener(clickListener);
+        ivTetrapak.setOnClickListener(clickListener);
+        ivGlassBottle.setOnClickListener(clickListener);
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        setMargins(flBarrioProgressMain, 0,0,(int)(defaultBarrio.getPuntos()%100 *dpWidth/100),0);
-        setMargins(flProfileProgressMain, 0,0,(int)(defaultProfile.getPuntos()%100 *dpWidth/100),0);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
     }
 
@@ -89,11 +161,14 @@ public class PickRecyclable extends AppCompatActivity {
         profileEmail.setText(googleSignInAccount.getEmail());*/
     }
     private void setMargins (View view, int left, int top, int right, int bottom) {
-        if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+        /*if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
             ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
 
             p.setMargins(left, top, right, bottom);
             view.requestLayout();
-        }
+        }*/
+        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) view.getLayoutParams();
+        lp.setMargins(left, top, right, bottom);
+        view.setLayoutParams(lp);
     }
 }
